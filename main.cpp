c8169b2f614eaa0dce12ff443e4dbc8e6385eafe
LinhNguyen0110 <91830412+LinhNguyen0110@users.noreply.github.com>
2021-10-05 02:27:13 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include "Combine_String.h"
+#include "gtest/gtest.h"
+
+int main(int argc,char* argv[]){
+  
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
